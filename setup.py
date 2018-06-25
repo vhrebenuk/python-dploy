750b899350fbdb56bfca9d8c663cc02e447b2630
Viktor Hrebeniuk <v.hrebeniuk@mobilunity.com>
2018-06-25 11:37:51 +0300

    fix setup

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 
 import os
 
+import sys
 from setuptools import setup, find_packages
 
 VERSION = '0.1.0'
@@ -10,6 +11,10 @@ VERSION = '0.1.0'
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
+fabtools = 'fabtools>=0.20.0'
+if sys.version_info >= (3, 0):  # substitute fabric3 for python 3 environments
+    fabtools = 'fabtools>=0.21.0'
 
 setup(
     name='dploy',
@@ -24,13 +29,13 @@ setup(
     include_package_data=True,
     zip_safe=True,
     scripts=['dploy/bin/python-dploy'],
+    dependency_links=['https://github.com/fabtools/fabtools#egg=fabtools-0.21.0'],
     install_requires=[
         'PyYAML>=3.12',
         'Jinja2>=2.9.5',
         'docopt>=0.6.2',
-        #'Fabric3==1.14.post1',
-        'fabric==1.14',
-        'fabtools>=0.20.0',
+        fabtools,
+
     ],
     classifiers=[
         'Environment :: Console',
